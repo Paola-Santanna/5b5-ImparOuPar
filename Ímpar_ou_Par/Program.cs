32c4dc694ee3c77fffc728a6af37782f508ba510
Paola-Santanna <paolinhasj@gmail.com>
2024-04-18 21:21:38 -0300

    Add project files.

---
+++ Ímpar_ou_Par/Program.cs
@@ -0,0 +1,14 @@
+﻿// 1.Faça um algorítmo que leia um número e informe de ele é par ou ímpar.
+
+int numero;
+
+Console.WriteLine("Digite um número inteiro: ");
+numero = int.Parse(Console.ReadLine());
+
+if (numero % 2 == 0)
+{
+    Console.WriteLine($"O número {numero} é PAR!");
+} else
+{
+    Console.WriteLine($"O número {numero} é ÍMPAR!");
+}
